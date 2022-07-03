76750b411d02eaacd0f98003d585aac267851a3e
napthedev <phongna.dev@gmail.com>
2022-07-03 21:28:47 +0700

    Feat: add demo page

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -1,7 +1,32 @@
 import type { NextPage } from "next";
+import Script from "next/script";
 
 const Demo: NextPage = () => {
-  return <div>{process.env.NEXT_PUBLIC_DEMO_SITE_ID}</div>;
+  if (!process.env.NEXT_PUBLIC_DEMO_SITE_ID)
+    return <div>Demo site ID hasn&apos;t been specified</div>;
+
+  return (
+    <div className="flex justify-center">
+      <div className="w-full max-w-xl my-8">
+        <div className="flex items-center gap-4 my-4">
+          <img className="w-12 h-12" src="/icon.png" alt="" />
+          <h1 className="text-3xl">CmtIO</h1>
+        </div>
+        <p>A comment system alternative to Disqus</p>
+
+        <div className="border border-zinc-800 mt-8" id="cmtio"></div>
+
+        <Script
+          src="/client.js"
+          data-site-id={process.env.NEXT_PUBLIC_DEMO_SITE_ID}
+          onLoad={() => {
+            // @ts-ignore
+            window.initCmtioIframe(document.getElementById("cmtio"));
+          }}
+        ></Script>
+      </div>
+    </div>
+  );
 };
 
 export default Demo;
