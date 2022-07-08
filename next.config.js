f4a16a25c01f7f36306920cce5ef60ccb94a51c8
napthedev <phongna.dev@gmail.com>
2022-07-08 13:06:22 +0700

    Chore: remove preact overwriting

--- next.config.js
+++ next.config.js
@@ -6,20 +6,6 @@ const withAnalyzer = require("@next/bundle-analyzer")({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack: (config, { isServer, dev }) => {
-    //  wait until preact is ready for react 18
-
-    // if (!dev && !isServer) {
-    //   config.resolve.alias = {
-    //     ...config.resolve.alias,
-    //     react: "preact/compat",
-    //     "react-dom/test-utils": "preact/test-utils",
-    //     "react-dom": "preact/compat",
-    //   };
-    // }
-
-    return config;
-  },
 };
 
 module.exports = withAnalyzer(nextConfig);
